29a49b93c58ca9d5f0e8f1fe77dfc8909252e170
“lmscj <1031749722@qq.com>
2020-09-25 19:31:06 +0800

    fix:修改登录注册样式和增加验证码组件

--- src/pages/index/Login.vue
+++ src/pages/index/Login.vue
@@ -1,24 +1,78 @@
 <template>
-  <div style="padding-top: 45px">
+  <div style="padding-top: 45px;">
     <el-row>
-      <el-col :span="13" :offset="1">
-        <div class="banner " style="margin-right: 0px;margin-left:20%;width:100%">
-          <img src="../../assets/img/logo-banner.jpeg" alt="" style="height: 420px">
+      <el-col :span="11" :offset="1">
+        <div class="banner" style="margin-right: 0px;margin-left:20%;width:100%">
+          <img src="../../assets/img/logo-banner.jpeg" alt style="height: 480px;min-width:800px" />
         </div>
       </el-col>
-      <el-col :span="8" :offset="1">
-        <div class="banner ">
-          <div style="text-align: center;font-size: 37px;font-weight: bold;padding-top: 95px">欢迎登录</div>
-          <div>
-            <el-form :model="form" :rules="loginRules" ref="form" label-width="100px" style="width: 80%;padding: 30px">
-              <el-form-item label="用户名" prop="key">
-                <el-input v-model="form.key"></el-input>
+      <el-col :span="9" :offset="2">
+        <div class="banner shadow">
+          <div style="text-align: center;font-size: 28px;font-weight: bold;padding-top:30px">欢迎登录</div>
+          <div class="form">
+            <el-form
+              :model="form"
+              :rules="loginRules"
+              ref="form"
+              style="width: 80%;padding-top:20px;"
+            >
+              <el-form-item prop="key">
+                <el-input placeholder="用户名" v-model="form.key"></el-input>
               </el-form-item>
-              <el-form-item label="密码" prop="password">
-                <el-input v-model="form.password" type="password"></el-input>
+              <el-form-item prop="password">
+                <el-input
+                  placeholder="密码"
+                  v-model="form.password"
+                  @keyup.enter.native="dispatchSubmit()"
+                  type="password"
+                ></el-input>
               </el-form-item>
-              <div style="display: flex;justify-content: center;padding-left: 100px;">
-                <el-button type="primary" style="width: 80%" @click="dispatchSubmit()">登录</el-button>
+              <el-form-item prop="code">
+                <el-row :span="24">
+                  <el-col :span="16" >
+                    <el-input placeholder="请输入验证码" v-model="form.code" type="text"></el-input>
+                  </el-col>
+                  <el-col :span="8">
+                    <div class="login-code" @click="refreshCode">
+                      <captcha-canvans :identifyCode="identifyCode" />
+                    </div>
+                  </el-col>
+                </el-row>
+              </el-form-item>
+              <el-form-item style="margin-top:-15px">
+                <el-checkbox class="auto-login">记住密码</el-checkbox>
+                <div style="float:right">
+                  <a href="javascript:;" style="padding: 1px 0px 0px 10px">忘记密码 ?</a>
+                </div>
+              </el-form-item>
+              <div style="margin-top:-10px;display: flex;justify-content: center;font-size:14px">
+                <el-button type="primary" style="width: 100%" @click="dispatchSubmit()">登录</el-button>
+              </div>
+              <div style="margin-top:10px;display: flex;justify-content: center;font-size:14px">
+                <el-button style="width: 100%" @click="$router.go(-1);">返回</el-button>
+              </div>
+              <div>
+                <el-divider></el-divider>
+              </div>
+              <div class="footer">
+                <div class="other">其它账号登录：</div>
+                <a>
+                  <img
+                    src="../../assets/img/wechat.png"
+                    alt
+                    style="height: 16px;width:16px;margin-right:5px;"
+                  />
+                </a>
+                <a>
+                  <img
+                    src="../../assets/img/qq.png"
+                    alt
+                    style="height: 16px;width:16px;margin-right:5px;"
+                  />
+                </a>
+                <a>
+                  <img src="../../assets/img/weibo.png" alt style="height: 16px;width:16px" />
+                </a>
               </div>
             </el-form>
           </div>
@@ -29,57 +83,158 @@
 </template>
 
 <script>
-    import {login} from '@/api/user'
-
-    export default {
-        name: "Login",
-        data() {
-            return {
-                form: {
-                    key: '',
-                    password: ''
-                },
-                loginRules: {
-                    key: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
-                }
-            }
-        },
-        methods: {
-            login() {
-                const loginInfo = {
-                    key: this.form.key,
-                    password: this.form.password,
-                    remember: true
-                };
-                login(loginInfo).then((res) => {
-                    localStorage.setItem("id", res.id)
-                    this.$router.push({path: "/"})
-                })
-            },
-            dispatchSubmit() {
-              this.$refs['form'].validate((valid) => {
-                if (valid) {
-                  this.login()
-                } else {
-                  this.$message({
-                    showClose: true,
-                    message: `用户名或密码不能为空`,
-                    type: 'error',
-                    isSingle: true
-                  });
-                  return false;
-                }
-              });
-            }
+import { login } from "@/api/user";
+import CaptchaCanvans from "@/components/Captcha/index";
+export default {
+  name: "Login",
+  components: {
+    CaptchaCanvans,
+  },
+  data() {
+    const validateCode = (rule, value, callback) => {
+      if (this.identifyCode !== value) {
+        this.form.code = "";
+        this.refreshCode();
+        callback(new Error("请输入正确的验证码"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      form: {
+        key: "",
+        password: "",
+        code: "",
+        loginStatus : false
+      },
+      identifyCode: "",
+      identifyCodes: "1234567890",
+      loginRules: {
+        key: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        code : [{validator: validateCode, trigger: 'blur'}],
+      },
+    };
+  },
+  created() {
+    this.refreshCode();
+  },
+  watch: {
+    identifyCode(v) {
+      this.loginStatus && (this.loginForm.code = v);
+    },
+  },
+  methods: {
+    login() {
+      const loginInfo = {
+        key: this.form.key,
+        password: this.form.password,
+        remember: true,
+      };
+      login(loginInfo).then((res) => {
+        localStorage.setItem("id", res.id);
+        this.$router.push({ path: "/" });
+      });
+    },
+    dispatchSubmit() {
+      this.loginStatus= true
+      this.$refs["form"].validate((valid) => {
+        if (valid) {
+          this.login();
+        } else {
+          this.$message({
+            showClose: true,
+            message: `   您的用户账号密码或验证码有误，请重新输入`,
+            type: "warring",
+            isSingle: true,
+          });
+          return false;
         }
-    }
+      });
+      this.loginStatus = false
+    },
+    randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    refreshCode() {
+      this.identifyCode = "";
+      this.makeCode(this.identifyCodes, 4);
+    },
+    makeCode(o, l) {
+      for (let i = 0; i < l; i++) {
+        this.identifyCode += this.identifyCodes[
+          this.randomNum(0, this.identifyCodes.length)
+        ];
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-  .banner {
-    height: 450px;
-    overflow: hidden;
-  }
+.banner {
+  margin-left: 150px;
+  height: 480px;
+  overflow: hidden;
+  min-width: 360px;
+}
+.form {
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  min-width: 360px;
+}
+.register {
+  padding: 1px 10px 0;
+  border-right: 1px solid #ccc;
+}
+a {
+  text-decoration: none;
+}
+a:link {
+  color: blue;
+}
+a:visited {
+  color: blue;
+}
+a:hover {
+  color: rgb(107, 0, 128);
+}
+a:active {
+  color: blue;
+}
+.auto-login {
+  position: absolute;
+  top: 0px;
+  left: 2px;
+  
+}
+.other {
+  width: auto;
+  color: #bbb;
+  font-size: 12px;
+  cursor: default;
+  
+}
+.login-code {
+  cursor: pointer;
+}
+.footer {
+  display: flex;
+  flex-direction: row;
+  margin-top: -10px;
+}
+.shadow {
+  border-radius: 10px;
+  -webkit-box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+  -moz-box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+  box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+}
 </style>
--- src/pages/index/Register.vue
+++ src/pages/index/Register.vue
@@ -1,24 +1,40 @@
 <template>
   <div style="padding-top: 45px">
     <el-row>
-      <el-col :span="13" :offset="1">
-        <div class="banner " style="margin-right: 0px;margin-left:20%;width:100%">
-          <img src="../../assets/img/logo-banner.jpeg" alt="" style="height: 420px">
+      <el-col :span="11" :offset="1">
+        <div class="banner" style="margin-right: 0px;margin-left:20%;width:100%">
+          <img src="../../assets/img/logo-banner.jpeg" alt style="height: 420px;min-width:800px" />
         </div>
       </el-col>
-      <el-col :span="7" :offset="1">
-        <div class="banner ">
-          <div style="text-align: center;font-size: 37px;font-weight: bold;padding-top: 95px">欢迎注册</div>
-          <div>
-            <el-form :model="form" :rules="registerRules" ref="form" label-width="100px" style="width: 80%;padding: 30px">
-              <el-form-item label="用户名" prop="key">
-                <el-input v-model="form.key"></el-input>
+      <el-col :span="9" :offset="2">
+        <div class="banner shadow">
+          <div style="text-align: center;font-size: 28px;font-weight: bold;padding-top:30px">欢迎注册</div>
+          <div class="form">
+            <el-form
+              :model="form"
+              :rules="registerRules"
+              ref="form"
+              style="width: 80%;padding: 20px"
+            >
+              <el-form-item prop="key">
+                <el-input placeholder="请输入用户名" v-model="form.key"></el-input>
               </el-form-item>
-              <el-form-item label="密码" prop="password">
-                <el-input v-model="form.password" type="text"></el-input>
+              <el-form-item prop="password">
+                <el-input placeholder="请输入密码" v-model="form.password" type="password" show-password></el-input>
               </el-form-item>
-              <div style="display: flex;justify-content: center;padding-left: 100px;">
-                <el-button type="primary" style="width: 80%" @click="dispatchSubmit()">注册</el-button>
+              <el-form-item prop="Repassword">
+                <el-input placeholder="请输入确认密码" v-model="form.Repassword" type="password" show-password></el-input>
+              </el-form-item>
+              <el-form-item style="margin-top:-10px">
+                <el-checkbox class="agree"  v-model="form.agreement">
+                  我已阅读并同意遵守
+                  <a href="javascript:;">法律声明</a> 和
+                  <a href="javascript:;">隐私条款</a>
+                </el-checkbox>
+                    <glimpse-canvans :code="form.code" />
+              </el-form-item>
+              <div style="display: flex;justify-content: center;font-size:14px;margin-top:-10px">
+                <el-button type="primary" style="width: 100%" @click="dispatchSubmit()">注册</el-button>
               </div>
             </el-form>
           </div>
@@ -29,70 +45,134 @@
 </template>
 
 <script>
-    import {register, login} from '@/api/user'
+import { register, login } from "@/api/user";
 
-    export default {
-        name: "Register",
-        data() {
-            return {
-                form: {
-                    key: '',
-                    password: ''
-                },
-                registerRules: {
-                    key: [
-                        {required: true, message: '请输入用户名', trigger: 'blur'},
-                        {min: 6, max: 50, message: '长度在6到50之间', trigger: 'blur'}
-                    ],
-                    password: [{required: true, message: '请输入密码', trigger: 'blur'},
-                        {min: 8, max: 32, message: '长度在8到32之间', trigger: 'blur'}
-                    ],
-                },
-            }
-        },
-        methods: {
-            register() {
-                register({...this.form, username: this.form.key}).then(() => {
-                    this.$message({
-                        showClose: true,
-                        message: `注册成功`,
-                        type: 'success',
-                        isSingle: true
-                    });
-                    const loginInfo = {
-                        key: this.form.key,
-                        password: this.form.password,
-                        remember: true
-                    };
-                    login(loginInfo).then((res) => {
-                        localStorage.setItem("id", res.id)
-                        this.$router.push({path: "/"})
-                    })
-                })
-            },
-            dispatchSubmit() {
-              this.$refs['form'].validate((valid) => {
-                if (valid) {
-                    this.register()
-                } else {
-                  this.$message({
-                    showClose: true,
-                    message: `注册信息不符合要求，请重新输入`,
-                    type: 'error',
-                    isSingle: true
-                  });
-                  return false;
-                }
-              });
-            }
+export default {
+  name: "Register",
+  data() {
+    var vaildRepassword = (rule, value, callback) => {
+      if (value !== this.form.password) {
+        return callback(new Error("两次密码不一致"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      form: {
+        key: "",
+        password: "",
+        Repassword: "",
+        agreement : false,
+      },
+      registerRules: {
+        key: [
+          { required: true, message: "请输入用户名", trigger: ["blur","change"] },
+          { min: 6, max: 50, message: "长度在6到50之间", trigger: ["blur"] },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: ["blur","change"] },
+          { min: 8, max: 32, message: "长度在8到32之间", trigger: ["blur"] },
+        ],
+        Repassword: [
+          { required: true, message: "请输入重复密码", trigger: ["blur","change"] },
+          { validator: vaildRepassword, trigger: ["blur"] },
+        ],
+      },
+    };
+  },
+
+  methods: {
+    register() {
+      register({ ...this.form, username: this.form.key }).then(() => {
+        this.$message({
+          showClose: true,
+          message: `注册成功`,
+          type: "success",
+          isSingle: true,
+        });
+        const loginInfo = {
+          key: this.form.key,
+          password: this.form.password,
+          remember: true,
+        };
+        login(loginInfo).then((res) => {
+          localStorage.setItem("id", res.id);
+          this.$router.push({ path: "/" });
+        });
+      });
+    },
+    dispatchSubmit() {
+      if(!this.form.agreement){
+          this.$message({
+            type : 'warning',
+            message : "请您先同意打勾我们的相关注册协议！",
+            isSingle: true,
+            showClose: true
+          })
+      }
+      else{
+      this.$refs["form"].validate((valid) => {
+        if (valid) {
+          this.register();
+        } else {
+          this.$message({
+            showClose: true,
+            message: `注册信息不符合要求，请重新输入`,
+            type: "error",
+            isSingle: true,
+          });
+          return false;
         }
-    }
+      });        
+      }
+
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-  .banner {
-    height: 500px;
-
-  }
+.banner {
+  margin-left: 150px;
+  height: 420px;
+  overflow: hidden;
+  min-width: 360px;
+}
+.form {
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  min-width: 360px;
+}
+.agree {
+  padding-top:-20px;
+}
+a {
+  text-decoration: none;
+}
+a:link {
+  color: blue;
+}
+a:visited {
+  color:  blue;
+}
+a:hover {
+  color: green;
+}
+a:active {
+  color: blue;
+}
+.shadow {
+  border-radius: 10px;
+  -webkit-box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+  -moz-box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+  box-shadow: 0 9px 30px -6px rgba(0, 0, 0, 0.2),
+    0 18px 20px -10px rgba(0, 0, 0, 0.04), 0 18px 20px -10px rgba(0, 0, 0, 0.04),
+    0 10px 20px -10px rgba(0, 0, 0, 0.04);
+}
 </style>
